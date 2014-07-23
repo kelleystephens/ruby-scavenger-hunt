0e1e2da795c7a66f58f706deb6e0cd89229311a8
Kelley Stephens <kelley.stephens@outlook.com>
2014-07-23 13:31:14 -0500

    added Jurnell's solutions to comments

--- lib/new_integer.rb
+++ lib/new_integer.rb
@@ -8,3 +8,12 @@ class Integer
     arr
   end
 end
+
+# Jurnell's solution for both factors and prime factors
+# def factors
+#   a = (2..self).to_a
+#   a.keep_if { |x| self%x == 0 && x != self }
+# end
+# def prime_factors
+#   self.factors.keep_if { |x| Prime.instance.prime(x) }
+# end
--- lib/vector.rb
+++ lib/vector.rb
@@ -35,3 +35,56 @@ end
 # MyVector[1,2].distance(Vector[2,2]) is how you would call the distance method
 # def distance(v)  -- v is the second vector passed in
 # self is the instance that distance is called on
+
+# -----Jurnell's solution
+# class MyVector < Vector
+#   def distance(v)
+#     Math.sqrt((self[0]-v[0])**2+(self[1]-v[1])**2)
+#   end
+#
+#   def self.distance(a, *more)
+#     if more.length < 1
+#       return nil
+#     end
+#     stash = []
+#     stash << a
+#     distance_stash = 0
+#     more.each do |v|
+#       stash << v
+#     end
+#     # correction student mentioned in class the bug of the first element
+#     # being summed twice. Hence running trough the loop one more time than
+#     # needed.
+# =begin
+#     for v in (0..stash.length-1)
+#       if v == 0
+#         distance_stash = distance_stash + a.distance(stash[v])
+#       else
+#         distance_stash = distance_stash + stash[v].distance(stash[v-1])
+#       end
+#     end
+# =end
+#     for v in (1..stash.length-1)
+#       if v == 1
+#         distance_stash = distance_stash + stash[0].distance(stash[v])
+#       else
+#         distance_stash = distance_stash + stash[v].distance(stash[v-1])
+#       end
+#     end
+#     distance_stash
+#   end
+#
+#   def <<(i)
+#     MyVector.elements(self.to_a << i)
+#   end
+#
+#   def self.angle(v1, v2)
+#     v1.angle(v2)
+#   end
+#
+#   def angle(v)
+#     numerator = self.inner_product(v)
+#     denominator = self.magnitude * v.magnitude
+#     Math::acos(numerator/denominator).round(10)
+#   end
+# end
